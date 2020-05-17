f645abc342f990d87f0bea36b40c50ac7ee1f967
nlg734 <npsm111@gmail.com>
2020-05-17 16:46:55 -0500

    first commit, stage 1 complete

---
+++ Coffee Machine/task/machine/coffee_machine.py
@@ -0,0 +1,12 @@
+# coffee_machine.py
+# written by Natasha Graham
+# JetBrains Python academy
+
+
+print("Starting to make a coffee")
+print("Grinding coffee beans")
+print("Boiling water")
+print("Mixing boiled water with crushed coffee beans")
+print("Pouring coffee into the cup")
+print("Pouring some milk into the cup")
+print("Coffee is ready!")
