39000706239ce3bbb06986f156a8de1cba3471bb
nlg734 <npsm111@gmail.com>
2020-05-17 16:53:20 -0500

    stage 2 complete

--- Coffee Machine/task/machine/coffee_machine.py
+++ Coffee Machine/task/machine/coffee_machine.py
@@ -3,10 +3,13 @@
 # JetBrains Python academy
 
 
-print("Starting to make a coffee")
-print("Grinding coffee beans")
-print("Boiling water")
-print("Mixing boiled water with crushed coffee beans")
-print("Pouring coffee into the cup")
-print("Pouring some milk into the cup")
-print("Coffee is ready!")
+def main():
+    print("Write how many cups of coffee you will need:")
+    n_cups = int(input())
+    print("For", n_cups, "cups of coffee you will need:")
+    print((n_cups * 200), "ml of water")
+    print((n_cups * 50), "ml of milk")
+    print((n_cups * 15), "g of coffee beans")
+
+
+main()
