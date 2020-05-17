068accf1bf8d4ec71a6e791978de411d8720053e
nlg734 <npsm111@gmail.com>
2020-05-17 17:04:39 -0500

    stage 3 complete

--- Coffee Machine/task/machine/coffee_machine.py
+++ Coffee Machine/task/machine/coffee_machine.py
@@ -3,13 +3,36 @@
 # JetBrains Python academy
 
 
+water_per = 200
+milk_per = 50
+beans_per = 15
+
+
 def main():
+    print("Write how many ml of water the coffee machine has:")
+    ml_water = int(input())
+    print("Write how many ml of milk the coffee machine has:")
+    ml_milk = int(input())
+    print("Write how many grams of coffee beans the coffee machine has:")
+    g_beans = int(input())
     print("Write how many cups of coffee you will need:")
     n_cups = int(input())
-    print("For", n_cups, "cups of coffee you will need:")
-    print((n_cups * 200), "ml of water")
-    print((n_cups * 50), "ml of milk")
-    print((n_cups * 15), "g of coffee beans")
 
+    count = 0
+
+    while ml_water > 0 and ml_milk > 0 and g_beans > 0:
+        ml_water -= water_per
+        ml_milk -= milk_per
+        g_beans -= beans_per
+        if ml_water >= 0 and ml_milk >= 0 and g_beans >= 0:
+         count += 1
+
+    if count == n_cups:
+        print("Yes, I can make that amount of coffee")
+    if count > n_cups:
+        print("Yes, I can make that amount of coffee (and even",
+              count - n_cups, "more than that)")
+    if count < n_cups:
+        print("No, I can only make", count, "cup(s) of coffee")
 
 main()
